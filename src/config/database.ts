c9a368bf48b7acb2ea916f0f38472f34e0dc492d
Eduardo Vedooto <vedootoeduardo@gmail.com>
2023-04-13 11:49:06 -0300

    feat: create prisma schema & prisma connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,20 +1,5 @@
-import pg from "pg";
-import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
 
-const { Pool } = pg;
+const prisma = new PrismaClient();
 
-type OptionsType = {
-  connectionString: string;
-  ssl: boolean;
-};
-
-const options: OptionsType = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: false,
-};
-
-if (process.env.MODE === "PROD") options.ssl = true;
-
-const connectionDb = new Pool(options);
-
-export default connectionDb;
+export default prisma;
